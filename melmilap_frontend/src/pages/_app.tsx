8b7e89a11842030446ed9c6b257b436e2fa743e1
Saish <saishmanandhar5@gmail.com>
2023-10-02 13:23:32 +0545

    commit

---
+++ melmilap_frontend/src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import "@/assets/scss/styles.scss";
+import DefaultLayout from "@/layouts/DefaultLayout";
+import type { AppProps } from "next/app";
+import { NextComponentType, NextPageContext } from "next";
+import MantineTheme from "@/plugins/theme";
+import Head from "next/head";
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextComponentType<NextPageContext, any, any> & {
+    Layout?: React.ComponentType<any>;
+  };
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const Layout = Component.Layout || DefaultLayout;
+  return (
+    <>
+      <Head>
+        <title>melmilap.com</title>
+      </Head>
+      <MantineTheme>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineTheme>
+    </>
+  );
+}
